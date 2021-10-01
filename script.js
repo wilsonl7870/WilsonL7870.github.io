e7de8ff7baa34d6917d42de01e0846d066b72ddf
wilsonl7870 <91745133+wilsonl7870@users.noreply.github.com>
2021-10-01 11:31:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Wilson Li"
+let userName = "wilsonl7870";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
